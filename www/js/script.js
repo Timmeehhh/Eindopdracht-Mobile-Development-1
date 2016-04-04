a9897b1ba0a52dcbcb0dfcc566e1f9a22cd18e32
Tim <timvbeers@icloud.com>
2016-04-04 21:41:25 +0200

    Navigeren

--- www/js/script.js
+++ www/js/script.js
@@ -294,7 +294,7 @@ $(function()
 			// mobile view
 			$(".tabletView").html('<table width="100%"><tr class="tr_header"><td colspan="2">'+data.name+'</td></tr>'+showImage+'<td>Adres:</td></tr><tr class="tr_header"><td colspan="2">Beschrijving</td></tr><tr class="tr_content" id="description"><td colspan="2">'+dataDescription+'</td></tr><tr class="tr_header"><td colspan="2">Contactinformatie</td></tr></table>');
 			$("html, body").animate({ scrollTop: 0 }, "slow");
-			$(".insertExtraInfo").html('<table id="detailTable"><tr class="tr_header"><td colspan="2">'+data.name+'</td></tr>'+showImage+'<tr class="tr_header"><td colspan="2">Beschrijving</td></tr><tr class="tr_content" id="description"><td colspan="2">'+dataDescription+'</td></tr><tr class="tr_header"><td colspan="2">Contactinformatie</td></tr><tr class="tr_content"><td colspan="2">' + contactHTML +  '</td></tr><tr class="tr_header"><td colspan="2">Stuur ons uw foto</td></tr><tr class="tr_content"><td colspan="2"></tr></table>');
+			$(".insertExtraInfo").html('<table id="detailTable"><tr class="tr_header"><td colspan="2">'+data.name+'</td></tr>'+showImage+'<tr class="tr_header"><td colspan="2">Beschrijving</td></tr><tr class="tr_content" id="description"><td colspan="2">'+dataDescription+'</td></tr><tr class="tr_header"><td colspan="2">Contactinformatie</td></tr><tr class="tr_content"><td colspan="2">' + contactHTML +  '</td></tr><tr class="tr_header"><td colspan="2">Navigeer naar dit restaurant</td></tr><tr class="tr_content"><td colspan="2"><a href="#" class="extern" uri="http://maps.apple.com/?q=' + address.street + ', ' + address.zipcode + ' ' +  address.city + '">Klik hier om te navigeren</a></tr></table>');
 			hideLoading();
         });
         
